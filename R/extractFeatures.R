f112f928ab6dc9d87ace930c9884c21bd12ab0c9
Killian Murphy <killian.murphy@york.ac.uk>
2022-03-30 14:27:56 +0100

    Working boundary coordinate preparation

--- R/extractFeatures.R
+++ R/extractFeatures.R
@@ -1,7 +1,31 @@
 prepareFeatureTable = function(featureTableFile, cellID) {
-  featureTableData = read.csv(featureTableFile, check.names=FALSE, skip = 1)
+  featureTableData = read.csv(featureTableFile, check.names = FALSE, skip = 1)
   colnames(featureTableData) = unlist(lapply(names(featureTableData), function(x) gsub(' \\(.+$', '', x)))
   return (as.matrix(subset(featureTableData, featureTableData$"Tracking ID" == cellID)))
 }
 
+prepareBoundaryCoordinates = function(roiArchive, cellID) {
+  roiFullFileList = unzip(roiArchive, list = TRUE)
+
+  grepString = paste('*-', cellID, '.roi', sep = '')
+  roiSubsetFileList = grep(grepString, roiFullFileList$"Name", value = TRUE)
+  unzip(roiArchive, files = roiSubsetFileList)
+  
+  orderedRoiSubsetFileList = roiSubsetFileList[order(as.numeric(sub("([0-9]+)\\-.*\\.roi", "\\1", roiSubsetFileList)))]
+
+  prepareRoiOutput = function(roiFile) {
+    roi = RImageJROI::read.ijroi(roiFile)
+    
+    frameID = sub("([0-9]+)\\-.*", "\\1", roi$"name")
+    numberOfCoordinates = roi$"n"
+    coordinates = c(t(roi$"coords"))
+    
+    return (c(frameID, cellID, numberOfCoordinates, coordinates))
+  }
+
+  boundaryCoordinates = lapply(orderedRoiSubsetFileList, prepareRoiOutput)
+  lapply(orderedRoiSubsetFileList, unlink)
+
+  return (boundaryCoordinates)
+}
 # TODO: add number of frames calculation
