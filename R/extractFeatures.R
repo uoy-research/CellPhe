05fe82660fb9a2eadf737a8c2a8ade9ccbb1045b
Killian Murphy <killian.murphy@york.ac.uk>
2022-04-11 21:00:41 +0100

    Got the right image output format

--- R/extractFeatures.R
+++ R/extractFeatures.R
@@ -3,19 +3,19 @@
 prepareFeatureTable = function(featureTableFile, cellID) {
   featureTableData = read.csv(featureTableFile, check.names = FALSE, skip = 1)
   colnames(featureTableData) = unlist(lapply(names(featureTableData), function(x) gsub(' \\(.+$', '', x)))
-  return (as.matrix(subset(featureTableData, featureTableData$"Tracking ID" == cellID)))
+  return (as.matrix(subset(featureTableData, featureTableData[["Tracking ID"]] == cellID)))
 }
 
 readRois = function(roiArchive, cellID) {
   roiFullFileList = unzip(roiArchive, list = TRUE)
 
   grepString = paste('*-', cellID, '.roi', sep = '')
-  roiSubsetFileList = grep(grepString, roiFullFileList$"Name", value = TRUE)
+  roiSubsetFileList = grep(grepString, roiFullFileList[["Name"]], value = TRUE)
   unzip(roiArchive, files = roiSubsetFileList)
   
   orderedRoiSubsetFileList = roiSubsetFileList[order(as.numeric(sub("([0-9]+)\\-.*\\.roi", "\\1", roiSubsetFileList)))]
   rois = lapply(orderedRoiSubsetFileList, RImageJROI::read.ijroi)
-  rois = lapply(rois, function(roi) {roi[["cellId"]] = sub(".*-", "", roi[["name"]]); roi[["frameId"]] = sub("-.*", "", roi[["name"]]); return (roi)})
+  rois = lapply(rois, function(roi) {roi[["cellId"]] = as.numeric(sub(".*-", "", roi[["name"]])); roi[["frameId"]] = as.numeric(sub("-.*", "", roi[["name"]])); return (roi)})
   lapply(orderedRoiSubsetFileList, unlink)
 
   return (rois)
@@ -47,13 +47,11 @@ readTiffs = function(directory) {
 applyRoiMask = function(roi, frame) {
   intensities = matrix(ncol = 3)
 
-  for (i in roi$left : roi$right) {
-    for (j in roi$top : roi$bottom) {
-      if (ptinpoly::pip2d(roi$coords, cbind(i, j)) %in% c(0, 1)) {
-        intensities = rbind(intensities, cbind(i, j, frame[j + 1, i + 1]))
-      }
-
-      if (ptinpoly::pip2d(roi$coords, cbind(i, j)) %in% c(0, 1)) {
+  for (i in roi[["left"]] : roi[["right"]]) {
+    for (j in roi[["top"]] : roi[["bottom"]]) {
+      if (ptinpoly::pip2d(roi[["coords"]], cbind(i, j)) %in% c(0, 1)) {
+        intensities = rbind(intensities, cbind(i, j, frame[["intensities"]][j + 1, i + 1]))
+      } else if (ptinpoly::pip2d(roi[["coords"]], cbind(i, j)) == -1) {
         intensities = rbind(intensities, cbind(i, j, -1))
       }
     }
@@ -62,12 +60,12 @@ applyRoiMask = function(roi, frame) {
   intensities = na.omit(intensities)
   attributes(intensities)[["na.action"]] = NULL
 
-  intensities[ , 1] = intensities[ , 1] - roi$left
-  intensities[ , 2] = intensities[ , 2] - roi$top
-  intensities = cbind(intensities, apply(intensities, 1, function(row) return (row[[1]] + (roi$width * row[[2]]))))
+  intensities[ , 1] = intensities[ , 1] - roi[["left"]]
+  intensities[ , 2] = intensities[ , 2] - roi[["top"]]
+  intensities = cbind(intensities, apply(intensities, 1, function(row) return (row[[1]] + (roi[["width"]] * row[[2]]))))
   intensities = intensities[order(intensities[ , 4]), ]
   
-  return (c(frame, roi$width, roi$height, intensities[ , 3]))
+  return (c(frame[["frameId"]], roi[["width"]], roi[["height"]], intensities[ , 3]))
 
   return (intensities)
 }
