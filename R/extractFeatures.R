bb8d0c095e01cd6ee52dc0adafc174c1f75c7633
Stuart Lacy <stuart.lacy@york.ac.uk>
2022-07-28 16:20:40 +0100

    Bug fix

--- R/extractFeatures.R
+++ R/extractFeatures.R
@@ -340,7 +340,7 @@ extractFeatures = function(df, frames, roi_folder, min_frames, framerate=1){
 	for (j in 1:n_cells){
 	  # TODO isn't this calculating mean frame id?
 	  # This could also be moved into the upper loop
-	  meanr[j] = mean(RandA[[j]][,1], na.rm = TRUE)
+	  meanr[j] = mean(RandA[[j]][,2], na.rm = TRUE)
 	}	
 	meanrad = mean(meanr)
 	
