34363b81e0700d185bce13ecda5cfd93bb29c6d7
llwiggins <91545985+llwiggins@users.noreply.github.com>
2022-08-24 10:36:49 +0100

    Updated prepareSegmentationTrainingSet function

--- R/prepareSegmentationTrainingSet.R
+++ R/prepareSegmentationTrainingSet.R
@@ -1,25 +1,19 @@
 #' Prepare a training set for classification of cell segmentation errors
 #'
 #'
-#' @description This function prepares a training set that can be used for classication of cell segmentation errors. This includes use of the
-#'\code{smotefamily::SMOTE()} function to oversample the minority class to avoid biased classifier training. This function outputs feature tables
-#'for ground truth segmentation errors and correctly segmented cells (with smote data included for the previously undersampled class), as 
+#' @description This function prepares a training set that can be used for classification of cell segmentation errors. This includes use of the
+#'\code{smotefamily::SMOTE()} function to over-sample the minority class to avoid biased classifier training. This function outputs feature tables
+#'for ground truth segmentation errors and correctly segmented cells (with smote data included for the previously under-sampled class), as 
 #'well as a list of ground truth labels for each class.
 #'
 #'
 #' @param segerrors A feature table of ground truth segmentation errors
 #' @param correctsegs A feature table of ground truth correctly segmented cells
 #'
-#' @return This function outputs the following and stores each as a global variable:
-#' @return \code{segerrordata} - Original feature table of ground truth segmentation errors plus synthetic segmentation errors if this was the undersampled class
-#' @return \code{correctsegdata} - Original feature table of ground truth correctly segmented cells plus synthetic segmentation errors if this was the undersampled class
-#' @return \code{segerrorlabels} - List of true class labels for the segmentation error class
-#' @return \code{correctseglabels} - List of true class labels for the correctly segmented class
-#'
-#' @examples prepareSegmentationTrainingSet(groundtruthsegerrors, groundtruthcorrectsegs)
+#' @return a list containing all information required for use with CellPhe functions for segmentation error prediction
+#' (\code{predictSegErrors} and \code{predictSegErrors_Ensemble}.)
 #' 
-#' @export
-#' 
+
 prepareSegmentationTrainingSet<-function(segerrors,correctsegs)
 {
   Segerrortraining <- segerrors
@@ -27,8 +21,8 @@ prepareSegmentationTrainingSet<-function(segerrors,correctsegs)
   
   ## collate correct segmentation and segmentation error data into one data frame
   data = rbind(Segerrortraining, Correctsegtraining) 
-  segerrordata<-Segerrortraining[,-c(1,2,3)] 
-  correctsegdata<-Correctsegtraining[,-c(1,2,3)]
+  segerrordata<-Segerrortraining[,-1] 
+  correctsegdata<-Correctsegtraining[,-1]
   alldata = rbind(segerrordata, correctsegdata)
   
   ## first column lists ground truth data labels
@@ -41,13 +35,11 @@ prepareSegmentationTrainingSet<-function(segerrors,correctsegs)
   smoteseg<-smotefamily::SMOTE(all[,-1], all$class, K = 3, dup_size = 1)
   
   ## add smote segmentation errors to the training sets and update segmentation error data table
-  smotesegsyn_data<-smoteseg$syn_data[,c(703, 1:702)] 
+  smotesegsyn_data<-smoteseg$syn_data[,c((dim(segerrors)[2]), 1:(dim(segerrors)[2]-1))] 
   all<-rbind(all, smoteseg$syn_data) 
   segdata<-rbind(segerrordata, smotesegsyn_data[,-1]) 
   newclass<-c(class, smotesegsyn_data[,1]) 
   class1 = rep("segerror", dim(segerrordata)[1])
-  assign("segerrordata", segerrordata, envir = .GlobalEnv)
-  assign("correctsegdata", correctsegdata, envir = .GlobalEnv)
-  assign("segerrorlabels", class1, envir = .GlobalEnv)
-  assign("correctseglabels", class2, envir = .GlobalEnv)
+  seginfo<-list(segerrordata, correctsegdata, class1, class2)
+  return(seginfo)
 }
