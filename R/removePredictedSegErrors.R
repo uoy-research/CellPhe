7bb52589a79379d9e384c4674a350a95d268f9da
llwiggins <91545985+llwiggins@users.noreply.github.com>
2022-08-24 11:04:29 +0100

    Update removePredictedSegErrors.R

--- R/removePredictedSegErrors.R
+++ R/removePredictedSegErrors.R
@@ -1,16 +1,15 @@
-## keep only correctly segmented cells
-#' Remove predicted segmentation errors from test set
+## Remove predicted segmentation errors from a data set
 #'
 #'@description This function can be used to automate removal of predicted segmentation errors from the test set
 #'
-#' @param testset Test set for segmentation error predictions to be made
+#' @param dataset Test set for segmentation error predictions to be made
 #' @param k Column index for the column of cell identifiers within the test set, e.g. if column 2 of the test set is a column of cell identifiers then here k = 2
 #' @param predictedSegErrors Output from either CellPhe::predictSegErrors() or CellPhe::predictSegErrors_Ensemble(), a list of cell identifiers for cells classified as segmentation error
 #'
-#' @examples removePredictedSegErrors(testset, 2, predicted_seg_errors)
+
 #' @export
-removePredictedSegErrors<-function(testset, k, predictedSegErrors)
+removePredictedSegErrors<-function(dataset, k, predictedSegErrors)
 {
-  testset<-subset(testset, testset[,k] %in% predictedSegErrors == FALSE)
+  testset<-subset(dataset, dataset[,k] %in% predictedSegErrors[,1] == FALSE)
   return(testset)
 }
