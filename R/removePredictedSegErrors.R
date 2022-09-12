9ec9a79ed3546bda9d3a789a3d8fe03e8ae703f4
Stuart Lacy <stuart.lacy@york.ac.uk>
2022-09-12 11:03:58 +0100

    Housekeeping docs

--- R/removePredictedSegErrors.R
+++ R/removePredictedSegErrors.R
@@ -1,12 +1,10 @@
-## Remove predicted segmentation errors from a data set
+#' Remove predicted segmentation errors from a data set
 #'
 #'@description This function can be used to automate removal of predicted segmentation errors from the test set
 #'
 #' @param dataset Test set for segmentation error predictions to be made
 #' @param k Column index for the column of cell identifiers within the test set, e.g. if column 2 of the test set is a column of cell identifiers then here k = 2
 #' @param predictedSegErrors Output from either predictSegErrors() or predictSegErrors_Ensemble(), a list of cell identifiers for cells classified as segmentation error
-#'
-
 #' @export
 removePredictedSegErrors<-function(dataset, k, predictedSegErrors)
 {
